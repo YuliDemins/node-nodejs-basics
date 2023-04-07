f034a2ce4f0da22816bc8200c830c725b097d3d3
Yulia <yudemins@mail.ru>
2023-04-07 18:43:22 +0300

    feat: add zip and workers

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import crypto from 'crypto';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
     // Write your code here 
+    fs.readFile(filePath, (error, data) => {
+        if  (error) {
+         throw Error('FS operation failed');
+     } else {
+        const hash = crypto.createHash('sha256').update(data).digest('hex');
+        process.stdout.write(hash);
+    }
+    })
 };
 
 await calculateHash();
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,35 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = path.join(__dirname, 'files');
+
 const compress = async () => {
     // Write your code here 
+    const readStream = fs.createReadStream(path.join(filePath, 'fileToCompress.txt'));
+    const writeStream = fs.createWriteStream(path.join(filePath, 'archive.gz'));
+    const gzip = zlib.createGzip();
+    readStream.pipe(gzip).pipe(writeStream);
+
+    readStream.on('error', (error) => {
+        if (error) {
+            throw Error('FS operation failed');
+        }
+    });
+    writeStream.on('error', (error) => {
+        if (error) {
+            throw Error('FS operation failed');
+        }
+    });
+    gzip.on('error', (error) => {
+        if (error) {
+            throw Error('FS operation failed');
+        }
+    });
 };
 
 await compress();
